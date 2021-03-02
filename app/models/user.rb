e46ca5876dfb9aeac1db59ebf71affe9f2587909
Tomohiro28 <tomohiro.1206@icloud.com>
2021-03-02 19:50:22 +0900

    新規登録実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,15 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+
+  validates :email,                 uniqueness: true
+  validates :password,              confirmation: true
+  validates :password_confirmation, presence: true  
+  validates :nickname,              presence: true
+  validates :last_name,             presence: true
+  validates :first_name,            presence: true
+  validates :birthday,              presence: true
+  validates :last_hurigana,         presence: true
+  validates :first_hurigana,        presence: true
 end
